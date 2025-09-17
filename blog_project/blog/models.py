c285217c06731eb0b9a31b40e6dbb05012e62aac
maka <maka.saparashvili@gmail.com>
2025-09-17 13:32:20 +0400

    Created App - Blog

--- blog_project/blog/models.py
+++ blog_project/blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class BlogPost(models.Model):
+    title = models.CharField(verbose_name="სათაური", max_length=255)
+    content = models.TextField(verbose_name="ტექსტი")
+    created_at = models.DateTimeField(verbose_name="შექმნის თარიღი",auto_now_add=True)
+    category = models.CharField(verbose_name="კატეგორია", max_length=255)
+    class Meta:
+        verbose_name = "Blog Post"
+        verbose_name_plural = "Blog Posts"
+
+    def __str__(self):
+        return self.title

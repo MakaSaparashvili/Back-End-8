067a593d0e503636246c862dcd53fd9082918a20
maka <maka.saparashvili@gmail.com>
2025-09-25 13:52:25 +0400

    Issue Fixed

--- blog_project/blog/models.py
+++ blog_project/blog/models.py
@@ -5,12 +5,11 @@ class BlogPost(models.Model):
     content = models.TextField(verbose_name="ტექსტი")
     created_at = models.DateTimeField(verbose_name="შექმნის თარიღი",auto_now_add=True)
     category = models.CharField(verbose_name="კატეგორია", max_length=255)
+
     class Meta:
         verbose_name = "Blog Post"
         verbose_name_plural = "Blog Posts"
         ordering = ['created_at']
-
-
 
     def __str__(self):
         return self.title
